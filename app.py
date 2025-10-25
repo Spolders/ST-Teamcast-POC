9bb1dee3ec8411ca4db8873790e6dce7d3892365
Spolders <erwin@flexup.pro>
2025-10-25 11:37:23 +0200

    Change x-axis to use 'date_label' in box plot

--- app.py
+++ app.py
@@ -54,16 +54,20 @@ else:
 
     fig_box = px.box(
         df_recent.dropna(subset=["Forecasted value"]),
-        x="Forecast date",
+        x="date_label",
         y="Forecasted value",
         points="all",
-        labels={"Date of Forecast (D-1)": "Date", "Forecasted value": "Forecast Hi-Lo Spread (€)"},
+        labels={
+            "date_label": "Date of Forecast (=D-1)",
+            "Forecasted value": "Forecast Hi-Lo Spread (€)",
+        },
         title="Distribution of Forecasted Day-Ahead Auction DE-LU Hi-Lo Spreads (Last 14 Days)",
         range_y=[
-        min(0, float(df_recent["Forecasted value"].min())),
-        max(0, float(df_recent["Forecasted value"].max())),
+            min(0, float(df_recent["Forecasted value"].min())),
+            max(0, float(df_recent["Forecasted value"].max())),
         ],
     )
+    
     fig_box.update_layout(xaxis_title="Date of Forecast (=D-1)", yaxis_title="DAA Hi-Lo Spread (€)")
     st.plotly_chart(fig_box, use_container_width=True)
 
